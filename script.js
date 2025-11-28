502f85ad0b926201b785474ce41de4b6cc4a06dc
AstinTabora <156150403+AstinTabora@users.noreply.github.com>
2025-11-28 16:19:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const toggleButton = document.getElementById('theme-toggle');
 const body = document.body;
+
+// default to image style (dark)
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme === 'light') {
+    body.classList.add('light-mode');
+    toggleButton.textContent = "Dark Mode";
+}
+
 toggleButton.addEventListener('click', () => {
-    body.classList.toggle('dark');
+    body.classList.toggle('light-mode');
+
+    // Update Button Text and Save Preference
+    if (body.classList.contains('light-mode')) {
+        toggleButton.textContent = "Dark Mode";
+        localStorage.setItem('theme', 'light');
+    } else {
+        toggleButton.textContent = "Light Mode";
+        localStorage.setItem('theme', 'dark');
+    }
 });
